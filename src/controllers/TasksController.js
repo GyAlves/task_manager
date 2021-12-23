0e442753df534d2aa9a591e4546ba079645069b9
GyAlves <gyasminassuncao@uni9.edu.br>
2021-12-23 19:07:27 -0300

    Controller finihed

--- src/controllers/TasksController.js
+++ src/controllers/TasksController.js
@@ -1,22 +1,63 @@
+const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-    res.send('getAllTasks')
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (error) {
+        res.status(404).json({ error });
+    }
 }
 
-const createTask = (req, res) => {
-    res.send('createTask')
+const createTask = async (req, res) => {
+    try {
+        const { name, completed } = req.body;
+        const task = await Task.create({ name, completed });
+        res.status(200).json({ task });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
 }
 
-const getTask = (req, res) => {
-    res.send('getTask')
+const getTask = async (req, res) => {
+    try {
+        const { task_id } = req.params;
+
+        const task = await Task.findOne({ _id: task_id });
+
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(400).json({ Error: error });
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send('updateTask')
+const updateTask = async (req, res) => {
+
+    try {
+        const { task_id } = req.params;
+        const { name, completed } = req.body;
+
+        const updatedTask = await Task.findByIdAndUpdate(task_id, { name, completed });
+
+        res.status(200).json({ message: "Task updated", data: updatedTask })
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+
 }
 
-const deleteTask = (req, res) => {
-    res.send('deleteTask')
+const deleteTask = async (req, res) => {
+
+    try {
+        const { task_id } = req.params;
+
+        const deleteTask = await Task.findByIdAndDelete(task_id);
+
+        res.status(200).json({ message: "Task deleted", data: deleteTask })
+
+    } catch (error) {
+        res.status(400).json({ error })
+    }
 }
 
 module.exports = {
--- src/models/Task.js
+++ src/models/Task.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 // Defines the  data structure 
 const TaskSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: [true, "Must provide name"],
+        trim: true,
+        maxLength: [20, "Name cannot be more than 20 characters"]
+    },
     completed: Boolean,
 })
 
 // The model is a wrapper for the schema, it provides an interface for creating, deleting ... 
-//the data
+// the data
 module.exports = mongoose.model("Task", TaskSchema)
